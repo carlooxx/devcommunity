3502377626e090a2b998ee7761f82ae3af30ab18
carlooxx <karlo.braovac@gmail.com>
2021-01-23 15:15:37 +0100

    Creating routes and connecting to DB

--- server.js
+++ server.js
@@ -1,5 +1,20 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+dotenv.config();
 const app = express();
+//Connect DB
+mongoose.connect(
+  process.env.DB_CONNECT,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  console.log("Connected to DB")
+);
+//Routes
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/auth", require("./routes/api/auth"));
+app.use("/api/profile", require("./routes/api/profile"));
+app.use("/api/posts", require("./routes/api/posts"));
 
 app.get("/", (req, res) => res.send("API running"));
 const PORT = process.env.PORT || 5000;
